17ce1b1ebe096f6d54ecf0cfe6ea439ae1c0c31e
lepetoc <lepetoctgp@outlook.fr>
2023-10-03 10:05:08 +0200

    gitignore .env
    Added ExpressJs with simple API and gitignore for .env file with api key

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,21 +1,16 @@
+require('dotenv').config()
 const { Client } = require('@googlemaps/google-maps-services-js');
+const express = require("express");
 
 const client = new Client({});
+const app = express();
+const port = 3000;
 
-const adresse = '20 Bd Général de Gaulle, 44200 Nantes'; // Adresse à géocoder
+app.get("/:adress", function (req, res) {
+  res.send("test\n" + process.env.TEST);
+});
 
-client.geocode({
-  params: {
-    address: adresse,
-    key: 'AIzaSyBF3zytez7dzeCMDOM8qDCXpBjauc83A2E', // Remplacez par votre clé API Google Maps
-  },
-})
-  .then((response) => {
-    const location = response.data.results[0].geometry.location;
-    console.log(`Coordonnées de ${adresse}:`);
-    console.log(`Latitude: ${location.lat}`);
-    console.log(`Longitude: ${location.lng}`);
-  })
-  .catch((error) => {
-    console.error('Erreur de géocodage:', error.response.data.error_message);
-  });
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+  console.log(process.env.GOOGLE_API_KEY);
+});
