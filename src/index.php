428c8cef9931760fa1d8e1f4528ec2512673d8c5
Lisachard <lisa.achard@ynov.com>
2023-10-03 15:35:02 +0200

    Launching front works as such as organizing files and folders ; separating footer, header and main index

--- src/index.php
+++ src/index.php
@@ -9,6 +9,9 @@
     <div class="container py-4 px-3 mx-auto">
       <h1>Hello, Bootstrap and Webpack!</h1>
       <button class="btn btn-primary">Primary button</button>
+      <?php 
+          include("footer.html"); 
+      ?>
     </div>
   </body>
 </html>
