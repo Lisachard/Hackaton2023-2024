d7e996ef09ef5741da908abc3af1a651d3935ab2
Lupitot <paul.piauger2@gmail.com>
2023-10-05 13:18:34 +0200

    Adding major front features : fixing loads of displaying bugs, adjusting css settings, glowing up pages

--- src/footer.php
+++ src/footer.php
@@ -4,19 +4,16 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Main page</title>
+    <title>Footer</title>
     <link rel="stylesheet" href="assets/style/commonStyle.css">
     <link rel="stylesheet" href="/src/assets/style/footer.css">
 </head>
 
-<body>
-    <div class="ground"></div>
-    <footer>
-        <div class="footer_container">
-            <div class="links"><a href="#">Crédits</a></div>
-            <div class="links"><a href="#">YnovCovoit © 2023 Tous droits réservés.</a></div>
-        </div>
-    </footer>
-</body>
+<section class=footer>
+    <div class="footerContainer">
+        <div class="links"><a href="#">Crédits</a></div>
+        <div class="links"><a href="#">Nudge © 2023 Tous droits réservés.</a></div>
+    </div>
+</section>
 
 </html>
--- src/form.php
+++ src/form.php
@@ -2,42 +2,27 @@
 <html>
 
 <head>
-    <link rel="stylesheet" type="text/css" href="/src/assets/style/commonStyle.css">
-    <link rel="stylesheet" type="text/css" href="/src/assets/style/form.css">
+    <link rel="stylesheet" type="text/css" href="./assets/style/commonStyle.css">
+    <link rel="stylesheet" type="text/css" href="./assets/style/form.css">
     <title>Formulaire de Recherche de Trajet</title>
 </head>
 
 <body>
     <h1>Recherche de Trajet</h1>
     <form action="traitement.php" method="post">
-        <input type="text" id="depart" name="depart" placeholder="Départ" required>
+        <div class="flex">
+            <input type="text" id="depart" name="depart" placeholder="Départ" required>
 
-        <input type="text" id="destination" name="destination" placeholder="Destination" required>
+            <input type="text" id="destination" name="destination" placeholder="Destination" required>
 
-        <input type="date" id="date" name="date" placeholder="Date">
+            <input type="date" id="date" name="date" placeholder="Date">
 
-        <div class="passenger-counter">
-            <button type="button" id="decrease-passengers">-</button>
-            <input type="number" id="passagers" name="passagers" value="1" min="1" required>
-            <button type="button" id="increase-passengers">+</button>
+            <div class="passenger-counter">
+                <button type="button" id="decrease-passengers">-</button>
+                <input type="number" id="passagers" name="passagers" value="1" min="1" required>
+                <button type="button" id="increase-passengers">+</button>
+            </div>
+            <input type="submit" value="Rechercher">
         </div>
-
-        <input type="submit" value="Rechercher">
     </form>
 </body>
-
-</html>
-
-<script>
-    document.getElementById('increase-passengers').addEventListener('click', function () {
-        var passengersInput = document.getElementById('passagers');
-        passengersInput.value = parseInt(passengersInput.value) + 1;
-    });
-
-    document.getElementById('decrease-passengers').addEventListener('click', function () {
-        var passengersInput = document.getElementById('passagers');
-        if (parseInt(passengersInput.value) > 1) {
-            passengersInput.value = parseInt(passengersInput.value) - 1;
-        }
-    });
-</script>
