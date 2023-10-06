b593b639ad6b12dc94c4c33720ab85c08772df56
Mostapha <mostourabi@gmail.com>
2023-10-06 09:14:33 +0200

    base de donnés fini

--- trajet.sql
+++ trajet.sql
@@ -1,19 +1,19 @@
 create table trajet (
 	id INT,
-	adresse VARCHAR(50),
-	heure TIME(50),
+	address VARCHAR(50),
+	time TIME(50),
 	date_trajet DATE(50),
-	nombre_de_passager VARCHAR(50),
+	number_of_passengers VARCHAR(50),
 	client INT,
 	direction Boolean
 );
-insert into trajet (id, adresse, heure, date_trajet, nombre_de_passager, client, direction) values (1, '13 Avenue De Coislin', '', '', '', '', '');
-insert into trajet (id, adresse, heure, date_trajet, nombre_de_passager, client, direction) values (2, '25 Rue Des Rosiers', '', '', '', '', '');
-insert into trajet (id, adresse, heure, date_trajet, nombre_de_passager, client, direction) values (3, '3 Route De Serpoliere', '', '', '', '', '');
-insert into trajet (id, adresse, heure, date_trajet, nombre_de_passager, client, direction) values (4, '18 Rue Du Clos Du Puits', '', '', '', '', '');
-insert into trajet (id, adresse, heure, date_trajet, nombre_de_passager, client, direction) values (5, '17 Route De Carrere', '', '', '', '', '');
-insert into trajet (id, adresse, heure, date_trajet, nombre_de_passager, client, direction) values (6, '30 Route De Clede', '', '', '', '', '');
-insert into trajet (id, adresse, heure, date_trajet, nombre_de_passager, client, direction) values (7, '9 Rue Neuve', '', '', '', '', '');
-insert into trajet (id, adresse, heure, date_trajet, nombre_de_passager, client, direction) values (8, '2 Rue St Joseph De Kermaria', '', '', '', '', '');
-insert into trajet (id, adresse, heure, date_trajet, nombre_de_passager, client, direction) values (9, '4 Rue Du Platane', '', '', '', '', '');
-insert into trajet (id, adresse, heure, date_trajet, nombre_de_passager, client, direction) values (10, '31 Avenue Des Francs Courtys', '', '', '', '', '');
+insert into trajet (id, address, time, date_trajet, number_of_passengers, client, direction) values (1, '13 Avenue De Coislin', '', '', '', '', '');
+insert into trajet (id, address, time, date_trajet, number_of_passengers, client, direction) values (2, '25 Rue Des Rosiers', '', '', '', '', '');
+insert into trajet (id, address, time, date_trajet, number_of_passengers, client, direction) values (3, '3 Route De Serpoliere', '', '', '', '', '');
+insert into trajet (id, address, time, date_trajet, number_of_passengers, client, direction) values (4, '18 Rue Du Clos Du Puits', '', '', '', '', '');
+insert into trajet (id, address, time, date_trajet, number_of_passengers, client, direction) values (5, '17 Route De Carrere', '', '', '', '', '');
+insert into trajet (id, address, time, date_trajet, number_of_passengers, client, direction) values (6, '30 Route De Clede', '', '', '', '', '');
+insert into trajet (id, address, time, date_trajet, number_of_passengers, client, direction) values (7, '9 Rue Neuve', '', '', '', '', '');
+insert into trajet (id, address, time, date_trajet, number_of_passengers, client, direction) values (8, '2 Rue St Joseph De Kermaria', '', '', '', '', '');
+insert into trajet (id, address, time, date_trajet, number_of_passengers, client, direction) values (9, '4 Rue Du Platane', '', '', '', '', '');
+insert into trajet (id, address, time, date_trajet, number_of_passengers, client, direction) values (10, '31 Avenue Des Francs Courtys', '', '', '', '', '');
